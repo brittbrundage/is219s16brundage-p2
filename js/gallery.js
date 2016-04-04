c410addd0ba88ff21a17bd98b2daabfbbfaca709
Brittani <bmb23@njit.edu>
2016-04-03 21:30:51 -0400

    Url to Image Json

--- js/gallery.js
+++ js/gallery.js
@@ -41,6 +41,8 @@ function swapPhoto() {
 	console.log(mCurrentIndex);
 }
 
+
+
 // Counter for the mImages array
 var mCurrentIndex = 0;
 
@@ -55,7 +57,7 @@ var mJson;
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = 'insert_url_here_to_image_json';
+var mUrl = $_GET["json"];
 
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
